13b1d56beb988fd65b1f6be9f886fc80fd9dc664
GuiReis <guilhermenerylindo@gmail.com>
2022-09-15 17:02:56 -0400

    if e elsef

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 20
+console.log(`A velocidade do seu carro é ${vel}hm/h`)
+if (vel > 60){     // condição simples !
+    console.log(`voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`dirija sempre usando cinto de segurança`)
+
