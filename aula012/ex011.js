bf1f53ec6f406bdd63a383bfeb097ec9c86f5bf8
GuiReis <guilhermenerylindo@gmail.com>
2022-09-20 18:36:37 -0400

    condições compostas

--- aula012/ex011.js
+++ aula012/ex011.js
@@ -1,9 +1,9 @@
-var idade = 15
+var idade = 65
 console.log(`voce tem ${idade} anos.`)
 if(idade < 16){
     console.log('não vota')
-} else if (idade < 18 || idade > 65){
-    console.log('voto opcional')
-}else {
-    console.log ('votar obrigatorio')
-}
+}else if (idade < 18 || idade > 65){
+       console.log('voto opcional')   
+    }else {
+        console.log('voto obrigatorio')
+    }
