537d6661007ec25404c8078c1b5bf6610049c609
GuiReis <guilhermenerylindo@gmail.com>
2022-09-22 19:11:16 -0400

    repetições

---
+++ aula013/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+do{
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 53)
+
+
+
+
+/*var c = 1
+while (c <= 500){
+    console.log(`passo ${c}`)
+    c++   // c = c + 1
+}
+*/
+/*
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+*/
